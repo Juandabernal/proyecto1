e8c78ea8acfc4dfd9da0be7bd7ae8fff3a0c7ddc
Juan David Bernal <juandavidbernal@192.168.0.5>
2022-11-12 20:56:59 -0500

    se agregó currenTime y audio.play

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 window.addEventListener('keydown', function(e) {
     const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if(!audio) return; //para la funcion hasta que corra toda
+    audio.currenTime = 0;
+    audio.play();
 });
