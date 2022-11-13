89d77ed66a614e7c80c69a076fce9774e7d6c9e9
Juan David Bernal <juandavidbernal@192.168.0.5>
2022-11-12 20:44:59 -0500

    se agrego un condicional de audio

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 window.addEventListener('keydown', function(e) {
-    const audio = this.document.querySelector(`audio[data-key=${e.keyCode}`)
+    const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return; //para la funcion hasta que corra toda
 });
