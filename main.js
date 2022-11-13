630b74ecb5bfde54f2fa30f56879badbd5072379
Juan David Bernal <juandavidbernal@192.168.0.5>
2022-11-12 21:05:23 -0500

    se creó el archivo estilos.css

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 window.addEventListener('keydown', function(e) {
     const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return; //para la funcion hasta que corra toda
-    audio.currenTime = 0;
+    audio.currenTime = 0; // tiempo de activacion 
     audio.play();
+    console.log(key)
 });
