5054ed4df3109bd0dac7db70d1999834a0a8b966
Juan David Bernal <juandavidbernal@192.168.0.5>
2022-11-13 17:16:58 -0500

    prse agrego un evento transition

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-window.addEventListener('keydown', function(e) {
+
+
+function playSound(e) {
     const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return; //para la funcion hasta que corra toda
-    audio.currenTime = 0; // tiempo de activacion 
+    audio.currentTime = 0; // tiempo de activacion 
     audio.play();
-    key.classList.add(`playing`);
-});
-
-const keys = document.querySelectorAll('.key');
-key.forEach(key => key.addEventListener('transitionend',)){
-    
+    key.classList.add('playing');
 };
+
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return; 
+    this.classList.remove('playing')
+
+
+};
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+window.addEventListener('keydown', playSound);
